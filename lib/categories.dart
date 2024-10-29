60e3c9c7777bf1025264ad3ec3463dbc6208be98
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2024-10-29 07:49:38 +0530

    GridView added

--- lib/categories.dart
+++ lib/categories.dart
@@ -8,6 +8,16 @@ class CategoriesScreen extends StatelessWidget{ //the categories should not be h
             appBar: AppBar(
                 title: const Text('Pick your category'),
             ),
+            body: GridView(
+                // can set the number of columns we want to have
+                // by default cross axis is top to bottom, but this is left to right and we get 2 colums
+                // we can get 2 columns horizontally 
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, 
+                    childAspectRatio: 3/2,
+                    crossAxisSpacing: 20,
+                ), 
+                children: [],),
         );
     }
 }
