bd6bcaef2cb1f788e887829fc18b9db942d38127
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2024-10-29 08:37:35 +0530

    texts added

--- lib/categories.dart
+++ lib/categories.dart
@@ -12,12 +12,20 @@ class CategoriesScreen extends StatelessWidget{ //the categories should not be h
                 // can set the number of columns we want to have
                 // by default cross axis is top to bottom, but this is left to right and we get 2 colums
                 // we can get 2 columns horizontally 
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 2, 
                     childAspectRatio: 3/2,
                     crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
                 ), 
-                children: [],),
+                children: const[
+                    Text('1', style: TextStyle(color: Colors.white),), 
+                    Text('2', style: TextStyle(color: Colors.white),),
+                    Text('3', style: TextStyle(color: Colors.white),),  
+                    Text('4', style: TextStyle(color: Colors.white),),
+                    Text('5', style: TextStyle(color: Colors.white),),
+                    Text('6', style: TextStyle(color: Colors.white),),
+                ],),
         );
     }
 }
