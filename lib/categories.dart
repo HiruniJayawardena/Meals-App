9119bdb77a823171fd85d9fedb6128f8d4b7372e
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2024-10-29 07:41:32 +0530

    Scaffold widget added

--- lib/categories.dart
+++ lib/categories.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 class CategoriesScreen extends StatelessWidget{ //the categories should not be handled with state
     const CategoriesScreen({super.key});
-    
+
     @override
     Widget build(BuildContext context) {
-        // TODO: implement build
-        throw UnimplementedError();
+        return Scaffold(
+            appBar: AppBar(
+                title: const Text('Pick your category'),
+            ),
+        );
     }
 }
