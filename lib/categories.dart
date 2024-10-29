2defddb912fe4b37d4eb69ff6bbb9c6bcf840532
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2024-10-29 07:37:32 +0530

    methods added to the class

---
+++ lib/categories.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+class CategoriesScreen extends StatelessWidget{ //the categories should not be handled with state
+    const CategoriesScreen({super.key});
+    
+    @override
+    Widget build(BuildContext context) {
+        // TODO: implement build
+        throw UnimplementedError();
+    }
+}
