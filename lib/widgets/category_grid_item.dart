dd4ea43d6197e0881cb276934b043090605fb4e5
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2024-10-31 11:28:36 +0530

    CaategorieGridItem added

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoriesGridItem extends StatelessWidget{
+  const CategoriesGridItem({super.key, required this.category});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
