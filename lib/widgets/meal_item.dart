d540bb75750606a80151a22af5858dd40edb35b6
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2024-11-05 00:19:38 +0530

    trasnparent_image added

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget{
   const MealItem({super.key, required this.meal});
@@ -11,7 +12,11 @@ class MealItem extends StatelessWidget{
     return Card(
       child: InkWell(
         onTap: (){},
-        
+        child: Stack(
+          children: [
+            FadeInImage(placeholder: MemoryImage(kTransparentImage), image: image),
+          ],
+        ),
       ),);
   }
 }
