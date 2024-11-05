6ef4f72280180e612bd81ec6bb1cf8e383631a24
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2024-11-05 09:49:22 +0530

    meal duration added

--- lib/widgets/meal_item_trait.dart
+++ lib/widgets/meal_item_trait.dart
@@ -12,7 +12,12 @@ class MealItemTrait extends StatelessWidget{
   
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Row(
+      children: [
+        Icon(icon, size: 17, color: Colors.white,),
+        const SizedBox(width: 6,),
+        Text(label, style: const TextStyle(color: Colors.white),)
+      ],
+    );
   }
 }
