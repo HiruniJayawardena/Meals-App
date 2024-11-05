065e45a148f2cdaea4c46b3ea44388ce8eaf1ee5
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2024-11-05 15:56:39 +0530

    steps added

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -36,6 +36,22 @@ class MealDetailsScreen extends StatelessWidget{
                 color: Theme.of(context).colorScheme.onSurface,
               ),
             ),
+          const SizedBox(height: 24,),
+          Text(
+            'Steps', 
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.primary,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 14,),
+          for(final step in meal.steps)
+            Text(
+              step,
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),  
         ],
       ),
     );
