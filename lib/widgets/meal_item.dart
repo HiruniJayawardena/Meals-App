6e10af0851fe0f72da047a4c01e1984bd1b8db47
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2024-11-05 09:59:43 +0530

    complexity text added

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -8,6 +8,10 @@ class MealItem extends StatelessWidget{
 
   final Meal meal;
 
+  String get complexityText{
+    return meal.complexity.name[0].toUpperCase() + meal.complexity.name.substring(1);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -55,9 +59,10 @@ class MealItem extends StatelessWidget{
                     const SizedBox(height: 12,),
                     Row(children: [
                       MealItemTrait(
-                        icon: Icons.schedule, 
-                        label: '${meal.duration} mins'
+                        icon: Icons.work, 
+                        label: '$complexityText mins'
                       ),
+                      const SizedBox(width: 12,),
                     ],),
                   ],),  
               ),
