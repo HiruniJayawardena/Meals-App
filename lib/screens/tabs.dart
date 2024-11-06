1949782f1b46b71481a7be17e51dca99a78a76d3
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2024-11-06 21:22:50 +0530

    selectedPageIndex variable set up

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/screens/categories.dart';
 
 class TabsScreen extends StatefulWidget{
   const TabsScreen({super.key});
@@ -10,15 +11,31 @@ class TabsScreen extends StatefulWidget{
 }
 
 class _TabsScreenState extends State<TabsScreen>{
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index){
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget active_page = const CategoriesScreen();
+
+    if(_selectedPageIndex == 1){}
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('dynamic....'),
       ),
-      body: ....,
+      body: ,
       bottomNavigationBar: BottomNavigationBar(
-        onTap: ,
+        onTap: (index){},
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.set_meal), label: 'Categories'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favouries'),
+        ],
       ),
     );
   }
