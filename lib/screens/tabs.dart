071d372d5729c63959e95f1a82b59b29534a1e40
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2024-12-06 09:56:15 +0530

    global variable created and selected filter results are assigned

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -5,6 +5,13 @@ import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/widgets/main_drawer.dart';
 
+const kInitialFilters = {
+  Filter.glutenFree : false,
+  Filter.lactoseFree : false,
+  Filter.vegetarian : false,
+  Filter.vegan : false
+};
+
 class TabsScreen extends StatefulWidget{
   const TabsScreen({super.key});
   
@@ -17,6 +24,8 @@ class TabsScreen extends StatefulWidget{
 class _TabsScreenState extends State<TabsScreen>{
   int _selectedPageIndex = 0;
   final List<Meal> _favouriteMeals = [];
+
+  Map<Filter, bool> _selectedFilters = kInitialFilters;
 
   void _showInfoMessage(String message){
     ScaffoldMessenger.of(context).clearSnackBars();
@@ -58,7 +67,10 @@ class _TabsScreenState extends State<TabsScreen>{
           builder: (context) => const FiltersScreen(),
         ),
       );
-      print(result);
+      
+      setState((){
+        _selectedFilters = result ?? kInitialFilters; // if the value is null, then the fallback value use
+      });
     }
   }
 
